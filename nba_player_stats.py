5e75d7da7987786b09174e93ea9d22cc6990c691
Raul Hernandez Jr <raul19812016@gmail.com>
2025-07-29 18:31:55 -0700

    Added positional information for each player. Adjusted the relevant columns. Added more print statements.

--- nba_player_stats.py
+++ nba_player_stats.py
@@ -21,6 +21,7 @@ def get_nba_player_stats(season='2024-25', season_type='Regular Season', league_
     ]].copy()
 
     # Get game averages and add them to the DataFrame
+    print("Doing per game calculations...")
     df_relevant['PPG'] = (df_relevant['PTS'] / df_relevant['GP']).round(1)
     df_relevant['APG'] = (df_relevant['AST'] / df_relevant['GP']).round(1)
     df_relevant['ORPG'] = (df_relevant['OREB'] / df_relevant['GP']).round(1)
@@ -39,11 +40,28 @@ def get_nba_player_stats(season='2024-25', season_type='Regular Season', league_
     df_relevant['FGPG'] = (df_relevant['FGM'] / df_relevant['GP']).round(1)
     df_relevant['FGAPG'] = (df_relevant['FGA'] / df_relevant['GP']).round(1)
 
+    # Get positional information for each player and add it to df_relevant
+    positions = []
+    print("Fetching player positions using player IDs...")
+    for i, row in df_relevant.iterrows():
+        player_id = row['PLAYER_ID']
+        try:
+            common_player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id, league_id_nullable=league_id_nullable)
+            df_common = common_player_info.get_data_frames()[0]
+            position = df_common.loc[0, 'POSITION']
+            positions.append(position)
+        except:
+            positions.append('N/A')
+        time.sleep(1)
+        if i % 100 == 0:
+            print(f"Processed {i} player's positions...")
+
+    df_relevant['POS'] = positions
 
     # Now we can drop the original columns that were used for calculations and keep only the relevant ones
     df_relevant = df_relevant[[
-        'PLAYER_ID', 'PLAYER_NAME', 'TEAM_ABBREVIATION', 'AGE', 'GP', 'MPG', 'FGPG', 'FGAPG', 'FG_PCT', 'FG3PG', 'FG3APG', 'FG3_PCT',
-        'FTPG', 'FTAPG', 'FT_PCT', 'ORPG', 'DRPG', 'RPG', 'APG', 'TPG', 'SPG', 'BPG', 'PFPG', 'FPPG', 'DD2', 'TD3'
+        'PLAYER_NAME', 'AGE', 'TEAM_ABBREVIATION', 'POS', 'GP', 'MPG', 'FGPG', 'FGAPG', 'FG_PCT', 'FG3PG', 'FG3APG', 'FG3_PCT',
+        'FTPG', 'FTAPG', 'FT_PCT', 'ORPG', 'DRPG', 'RPG', 'APG', 'SPG', 'BPG', 'TPG', 'PPG', 'PFPG', 'FPPG', 'DD2', 'TD3'
     ]]
     
     # Save to CSV
